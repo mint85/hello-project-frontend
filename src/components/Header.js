ad16ce92e987398a9aca1c24169b1d263dc0b384
kelsey nocek <kelseyn12@hotmail.com>
2022-01-31 16:03:18 -0600

    updating changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ function Header (props) {
     
     return (
         <nav>
+            <img src = "https://i.ibb.co/19PcHJ5/ee48d5829b2040aea1ff9af1e69d703a.png" alt='logo' style={{height:"150px", width: "auto"}} className='logo'/>
             <Link to="/" style={{textDecoration:"none", color:"black"}}>HOME
             </Link>
             <Link to="/ideas" style={{textDecoration:"none", color:"black"}}>IDEAS
