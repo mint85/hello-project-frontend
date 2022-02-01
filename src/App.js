94dadcef1ccb9a64f59b2fd1e2b0665c61807c98
Roby Martinez <martinez.robyp@gmail.com>
2022-01-31 19:14:07 -0700

    sets up New form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Route, Switch } from 'react-router-dom';
+import {useState, useEffect} from "react";
 
 import Home from './pages/Home';
 import Ideas from './pages/Ideas';
@@ -11,7 +12,36 @@ import './App.css';
 
 function App() {
 
+  // Create state to hold Ideas data
+   const [ideas, setIdeas] = useState(null);
+
   const URL = "https://hello-project-backend.herokuapp.com/";
+
+  // Create function to make api call to obtain Ideas data from database
+  const getIdeasData = async () => {
+    const response = await fetch(URL + "ideas");
+    const data = await response.json();
+    setIdeas(data);
+  };
+
+const createIdeasData = async(idea) => {
+  await fetch(URL + "ideas", {
+    method: "POST",
+    headers: {
+      "Content-Type": "Application/JSON"
+    },
+    body: JSON.stringify(idea)
+  });
+
+  getIdeasData();
+}
+
+
+
+  // Make initial call for data inside a useEffect
+  // This will only happen once on component load
+  useEffect(() => getIdeasData(), []);
+  
   return (
     <div className="App">
      <Header />
@@ -20,10 +50,10 @@ function App() {
          <Home />
        </Route>
        <Route path="/ideas">
-         <Ideas URL={URL} />
+         <Ideas ideas={ideas} />
        </Route>
        <Route path="/newform">
-         <NewForm />
+         <NewForm ideas={ideas} createIdeasData={createIdeasData}/>
        </Route>
      </Switch>
      <Footer />
