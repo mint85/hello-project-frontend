e38452b790fb44f825e8552954e20edeb6550d66
Allen Jones <allen.jones@gmail.com>
2022-02-01 16:55:07 +0000

    adds Ideas.css and adds additional classNames for css styling of Ideas.js page

--- src/pages/Ideas.js
+++ src/pages/Ideas.js
@@ -1,20 +1,26 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
+import "./Ideas.css";
+import "../styles.css";
 
 function Ideas(props) {
-
-
   // Function to return once we get the data from the backend
   const loaded = () => {
-    return props.ideas.map((idea) => (
-      <div key={idea._id} className="idea-container">
-        <p className="idea-title">{idea.name}</p>
-        <img src={idea.img} alt={idea.name} />
-        <div className="button-container">
-          <Link to={`/ideas/${idea._id}`}><button className="button">EDIT</button></Link>
-        </div>
-      </div>
-    ));
+    return (
+      <section className="ideas-flex-container">
+        {props.ideas.map((idea) => (
+          <div key={idea._id} className="idea-container">
+            <p className="idea-title">{idea.name}</p>
+            <img className="idea-image" src={idea.img} alt={idea.name} />
+            <div className="button-container">
+              <Link to={`/ideas/${idea._id}`}>
+                <button className="button">EDIT</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </section>
+    );
   };
 
   return props.ideas ? loaded() : <h1>Loading...</h1>;
